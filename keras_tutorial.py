866d3ac8f040768dbe5b15c2d42080a5dabe8b82
darshitpurohit101 <38689780+darshitpurohit101@users.noreply.github.com>
2020-01-23 11:56:10 +0530

    Update keras_tutorial.py
    shows the usage of two different architecture

--- keras_tutorial.py
+++ keras_tutorial.py
@@ -7,7 +7,7 @@ Created on Thu Jan 23 09:33:07 2020
 
 import tensorflow as tf
 
-tf.__version__
+#tf.__version__ # to know the version
 # 28x28 image of hand writen digit 0-9
 mnist = tf.keras.datasets.mnist 
 #load and split data
@@ -19,29 +19,61 @@ plt.imshow(x_train[0], cmap = plt.cm.binary)
 plt.show()
 #print(x_train)
 
+''' preprocessing of the data '''
+	
+x_train = x_train.reshape(x_train.shape[0], 1, 28, 28)
+x_test = x_test.reshape(x_test.shape[0], 1, 28, 28)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
 #normailze the data(scaling it)
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-#visuallzie after scaling
+print(x_train.shape)
+#visuallzing after scaling
 print(x_train)
 
-#model creation
+''' model creation [ model architecture] i have used 2 different types '''
+'''
+                                        one 
+'''
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-#last layer should have number of classfication in the dence layer
+#last layer should have number of classfication(10) in the dence layer
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax)) #for probability distiribution use softmmax
+
+'''
+                                        two
+'''
+#
+#model = tf.keras.models.Sequential()
+''' use convolution when input size is not set in prior, Otherwise just use dense. '''
+##model.add(tf.keras.layers.Convolution2D(32, 3, 3, activation=tf.nn.relu, input_shape=(1, 28, 28)))
+##model.add(tf.keras.layers.Convolution2D(32, 3, 3, activation=tf.nn.relu, input_shape=(1, 28, 28)))
+##model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+##model.add(tf.keras.layers.Dropout(0.25)) # to avoid overfitting
+## weights from the Convolution layers must be flattened (made 1-dimensional) 
+##before passing them to the fully connected Dense layer.
+#model.add(tf.keras.layers.Flatten()) 
+#model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+#model.add(tf.keras.layers.Dropout(0.25))
+#model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+#model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
 #u can use any optimizer(like gradient decent(back prop)) 
 model.compile(optimizer= 'adam', 
               loss='sparse_categorical_crossentropy',
               metrics = ['accuracy'])
 
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=8)
 
+#to know the accuracy on test set
 val_loss, val_acc = model.evaluate(x_test, y_test)
-#print(val_loss, val_acc)
+print(val_loss, val_acc)
 
 #save model
 model.save('myfirstmodel')
